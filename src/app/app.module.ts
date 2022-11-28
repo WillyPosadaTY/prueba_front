40e430ce134d78bb556aeae0a62727f03c849591
William1701520853 <52585477+William1701520853@users.noreply.github.com>
2022-11-28 18:29:44 -0500

    Componentes y servicios base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TransportComponent } from '../components/transport/transport.component';
+import { FlightComponent } from '../components/flight/flight.component';
+import { JourneyComponent } from '../components/journey/journey.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TransportComponent,
+    FlightComponent,
+    JourneyComponent
   ],
   imports: [
     BrowserModule,
